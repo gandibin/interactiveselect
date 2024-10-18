613f065b77244d99fa6990593cacb8931ff12603
gandibin <gandibin@qq.com>
2024-10-18 10:44:09 +0800

    thinkphp path add2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'topthink/think',
+        'pretty_version' => 'v5.1.41',
+        'version' => '5.1.41.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'topthink/framework' => array(
+            'pretty_version' => 'v5.1.42',
+            'version' => '5.1.42.0',
+            'reference' => 'ecf1a90d397d821ce2df58f7d47e798c17eba3ad',
+            'type' => 'think-framework',
+            'install_path' => __DIR__ . '/../../thinkphp',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'topthink/think' => array(
+            'pretty_version' => 'v5.1.41',
+            'version' => '5.1.41.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'topthink/think-installer' => array(
+            'pretty_version' => 'v2.0.5',
+            'version' => '2.0.5.0',
+            'reference' => '38ba647706e35d6704b5d370c06f8a160b635f88',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../topthink/think-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
