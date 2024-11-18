ca7b96a683746089c734639392669ea15834fcb0
gandibin <gandibin@qq.com>
2024-11-18 15:38:19 +0800

    添加筛选前的变动

--- runtime/temp/0401c4eb12f10cb106e83047a4d2ec68.php
+++ runtime/temp/0401c4eb12f10cb106e83047a4d2ec68.php
@@ -1,4 +1,4 @@
-<?php /*a:1:{s:74:"D:\phpstudy_pro\WWW\rn.ylsoled.com\application\index\view\index\index.html";i:1728853889;}*/ ?>
+<?php /*a:1:{s:74:"D:\phpstudy_pro\WWW\rn.ylsoled.com\application\index\view\index\index.html";i:1729421631;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,7 +24,27 @@
             margin-top: 20px;
             text-align: center;
         }
+
+        ul {
+            list-style: none; /* Remove bullet points */
+            padding: 0;
+            margin: 0;
+            display: flex; /* Make list items flex container */
+        }
+
+        li {
+            margin-right: 10px; /* Add spacing between list items */
+        }
+
+        /* Hide additional columns initially */
+        .more-column {
+            display: none;
+        }
     </style>
+
+ <!-- Include jQuery -->
+ <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 </head>
 <body>
     <h1>Minipc 产品列表</h1>
@@ -42,20 +62,22 @@
                 <th>RAM</th>
                 <th>ROM</th>
                 <th>GPU</th>
-                <th>Communicate</th>
-                <th>Operating Environment</th>
-                <th>Other Interface</th>
-                <th>NPU</th>
-                <th>Ethernet Port</th>
-                <th>Camera</th>
-                <th>Graphic Port</th>
-                <th>Replace</th>
-                <th>Link</th>
-                <th>Format Link</th>
-                <th>Link ID</th>
-                <th>Price Check</th>
-                <th>Type</th>
-                <th>Remark</th>
+                
+                <!-- More columns initially hidden -->
+                <th class="more-column">Communicate</th>
+                <th class="more-column">Operating Environment</th>
+                <th class="more-column">Other Interface</th>
+                <th class="more-column">NPU</th>
+                <th class="more-column">Ethernet Port</th>
+                <th class="more-column">Camera</th>
+                <th class="more-column">Graphic Port</th>
+
+                <th class="more-column">Format Link</th>
+
+                <th class="more-column">Type</th>
+                <th class="more-column">Remark</th>
+
+                <th>show more specification</th>
             </tr>
         </thead>
         <tbody>
@@ -72,28 +94,48 @@
                 <td><?php echo htmlentities($item['ram']); ?></td>
                 <td><?php echo htmlentities($item['rom']); ?></td>
                 <td><?php echo htmlentities($item['GPU']); ?></td>
-                <td><?php echo htmlentities($item['communicate']); ?></td>
-                <td><?php echo htmlentities($item['operating_environment']); ?></td>
-                <td><?php echo htmlentities($item['other_interface']); ?></td>
-                <td><?php echo htmlentities($item['NPU']); ?></td>
-                <td><?php echo htmlentities($item['ethernet_port']); ?></td>
-                <td><?php echo htmlentities($item['camera']); ?></td>
-                <td><?php echo htmlentities($item['graphic_port']); ?></td>
-                <td><?php echo htmlentities($item['replace']); ?></td>
-                <td><?php echo htmlentities($item['link']); ?></td>
-                <td><?php echo htmlentities($item['format_link']); ?></td>
-                <td><?php echo htmlentities($item['link_id']); ?></td>
-                <td><?php echo htmlentities($item['price_check']); ?></td>
-                <td><?php echo htmlentities($item['type']); ?></td>
-                <td><?php echo htmlentities($item['remark']); ?></td>
+                
+                <!-- More columns initially hidden -->
+                <td class="more-column"><?php echo htmlentities($item['communicate']); ?></td>
+                <td class="more-column"><?php echo htmlentities($item['operating_environment']); ?></td>
+                <td class="more-column"><?php echo htmlentities($item['other_interface']); ?></td>
+                <td class="more-column"><?php echo htmlentities($item['NPU']); ?></td>
+                <td class="more-column"><?php echo htmlentities($item['ethernet_port']); ?></td>
+                <td class="more-column"><?php echo htmlentities($item['camera']); ?></td>
+                <td class="more-column"><?php echo htmlentities($item['graphic_port']); ?></td>
+
+                <td class="more-column"><a href="<?php echo htmlentities($item['format_link']); ?>" target="_blank"> product link </a></td>
+                
+
+                <td class="more-column"><?php echo htmlentities($item['type']); ?></td>
+                <td class="more-column"><?php echo htmlentities($item['remark']); ?></td>
+                <td><button class="toggleMoreBtn">More>></button></td>
             </tr>
             <?php endforeach; endif; else: echo "" ;endif; ?>
         </tbody>
     </table>
 
+    
+
     <!-- 分页导航 -->
     <div class="pagination">
         <?php echo $paginationLinks; ?>
     </div>
+
+    <script>
+        // 使用 jQuery 切换列的可见性
+        $(document).ready(function(){
+            $('.toggleMoreBtn').on('click', function() {
+                // 切换所有带有 .more-column 的列的显示状态
+                $('.more-column').toggle();
+    
+                // 获取当前按钮的状态（是 More>> 还是 Less<<）
+                var newText = $(this).text() === 'More>>' ? 'Less<<' : 'More>>';
+    
+                // 更新所有带有 .toggleMoreBtn 类的按钮文本
+                $('.toggleMoreBtn').text(newText);
+            });
+        });
+    </script>
 </body>
 </html>
